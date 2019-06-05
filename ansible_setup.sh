3fb9e0b0a3156815a7782887a4bfd4a95b8fd2fc
YoungHo Kwak <jason.uphiller@gmail.com>
2019-06-05 17:50:58 +0900

    Update ansible_setup.sh

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -5,5 +5,5 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt update
-sudo apt install ansible
+sudo apt update -y
+sudo apt install ansible -y
