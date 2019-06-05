4d744183f651eb2dfcb4cc6118bdf4b29f6b9194
YoungHo Kwak <jason.uphiller@gmail.com>
2019-06-05 17:46:00 +0900

    Update ansible_setup.sh

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -2,7 +2,7 @@ sudo apt-get install dirmngr --install-recommends
 sudo apt-get install software-properties-common
 
 #Add the following line to /etc/apt/sources.list:
-deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+sudo add-apt-repository "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update
